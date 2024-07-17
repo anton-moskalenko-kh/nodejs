023653cd35d38f1672a664df64fbdf7b3c35901e
anton-moskalenko-kh <anton.moskalenko.kh@gmail.com>
2024-07-17 20:28:29 +0300

    lesson4 p1

--- src/fs.service.ts
+++ src/fs.service.ts
@@ -1,16 +1,16 @@
-import fs from 'node:fs/promises'
-import path from 'node:path'
+import fs from "node:fs/promises";
+import path from "node:path";
 
-const pathToDb = path.join(process.cwd(), 'db.json')
+const pathToDb = path.join(process.cwd(), "db.json");
 
 class FsService {
-    public async read() {
-        const json = await fs.readFile(pathToDb, 'utf-8')
-        return json ? JSON.parse(json) : []
-    }
-    public async write(users) {
-        await fs.writeFile(pathToDb, JSON.stringify(users))
-    }
+  public async read() {
+    const json = await fs.readFile(pathToDb, "utf-8");
+    return json ? JSON.parse(json) : [];
+  }
+  public async write(users) {
+    await fs.writeFile(pathToDb, JSON.stringify(users));
+  }
 }
 
-export const fsService = new FsService()
+export const fsService = new FsService();
